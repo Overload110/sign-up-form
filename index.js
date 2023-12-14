f109325dfce0cecd83dc37c4067ac8ee346752a7
Overload110 <149197288+Overload110@users.noreply.github.com>
2023-12-14 13:02:13 -0600

    Add form validation.

--- index.js
+++ index.js
@@ -19,6 +19,48 @@ function validateInputs(){
     const phoneValue = phone.value.trim();
     const passwordValue = password.value.trim();
     const confirmValue = confirmPass.value.trim();
+
+    if(firstNameValue === ""){
+        setError(firstName, "First name is required.");
+    }else{
+        setSuccess(firstName);
+    }
+    if(lastNameValue === ""){
+        setError(lastName, "Last name is required.");
+    }else{
+        setSuccess(lastName);
+    }
+    if(emailValue === ""){
+        setError(email, "Email is required.");
+    }else if(!isValidEmail(emailValue)){
+        setError(email, "Please enter a valid email address.");
+    }else{
+        setSuccess(email);
+    }
+
+    if(phoneValue === ""){
+        setError(phone, "Email is required.");
+    }else if(!isValidPhone(phoneValue)){
+        setError(phone, "Please enter a valid phone number.");
+    }else{
+        setSuccess(phone);
+    }
+
+    if(passwordValue === ""){
+        setError(password, "Password is required.");
+    }else if(passwordValue.length < 8){
+        setError(password, "Password must be at least eight characters long.");
+    }else{
+        setSuccess(password);
+    }
+
+    if(confirmValue === ""){
+        setError(confirmPass, "Please confirm your password.");
+    }else if(confirmValue !== passwordValue){
+        setError(confirmPass, "Passwords don't match.");
+    }else{
+        setSuccess(confirmPass);
+    }
 }
 
 function setError(element, message){
@@ -28,4 +70,23 @@ function setError(element, message){
     errorDisplay.innerText = message;
     inputControl.classList.add('error');
     inputControl.classList.remove('success');
+}
+
+function setSuccess(element){
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = "";
+    inputControl.classList.add('success');
+    inputControl.classList.remove('errror');
+}
+
+function isValidEmail(email){
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+function isValidPhone(phone){
+    const re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im
+    return re.test(String(phone).toLowerCase());
 }
